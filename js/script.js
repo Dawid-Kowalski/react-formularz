c028d1f0b2d66f1b56449e1950d4c46a6fa5fa5d
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-11 11:02:22 +0100

    Add form component

--- js/script.js
+++ js/script.js
@@ -9,12 +9,31 @@ class Title extends React.Component {
 	}
 }
 
+class ContactForm extends React.Component {
+	render() {
+		return (
+			<div>
+				<form>
+					<label>Imie</label>
+					<input type="text" placeholder="imie"/>
+					<label>Nazwisko</label>
+					<input type="text" placeholder="nazwisko"/>
+					<label>Email</label>
+					<input type="email" placeholder="email"/>					
+					<button type="submit">dodaj kontakt</button>
+				</form>
+			</div>
+		)
+	}
+} 
+
 class App extends React.Component {
 
 	render() {
 		return (
 			<div>
 				<Title title = 'Formularz'/>
+				<ContactForm />
 			</div>
 		)
 	}
